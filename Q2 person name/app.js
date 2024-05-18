3af7b4f5dc235e8e930dbae84f035c5cb4d1f5f0
altaf-sajdi <sajdiedu@gmail.com>
2024-05-17 22:33:18 -0700

    i created second qoustion from 45 assignment

---
+++ Q2 person name/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//Q2 - Personal Message: Store a person’s name in a variable,
+//and print a message to that person. Your message should be simple, 
+//such as, “Hello Eric, would you like to learn some Python today?”
+let personName = "Altaf Hussain Sajdi ,"; // stored a person name a variable ! 
+let meassage = `hello ${personName} would you like to learn some python today ?`; // i creat a variable and store it a frist varibale and message .
+console.log(meassage);
---
+++ Q2 person name/app.ts
@@ -0,0 +1,10 @@
+//Q2 - Personal Message: Store a person’s name in a variable,
+ //and print a message to that person. Your message should be simple, 
+ //such as, “Hello Eric, would you like to learn some Python today?”
+
+
+ let personName:string="Altaf Hussain Sajdi ,"; // stored a person name a variable ! 
+
+ let meassage:string=`hello ${personName} would you like to learn some python today ?`; // i creat a variable and store it a frist varibale and message .
+
+ console.log(meassage)
