cc66a2e57d43bee6f736b4d6f0ca9fef058d440e
altaf-sajdi <sajdiedu@gmail.com>
2024-05-18 00:19:50 -0700

    changed

--- Q3 name cases/app.js
+++ Q3 name cases/app.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let personName = "altaf sajdi";
 console.log(personName.toLowerCase());
 console.log(personName.toUpperCase());
-console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+console.log(personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' '));
--- Q3 name cases/app.ts
+++ Q3 name cases/app.ts
@@ -2,4 +2,4 @@ let personName :string="altaf sajdi";
 
 console.log(personName.toLowerCase());
 console.log(personName.toUpperCase());
-console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+console.log(personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
