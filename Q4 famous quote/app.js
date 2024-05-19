ea8f39f59939e80ebec510724ab36c65fd3a815d
altaf-sajdi <sajdiedu@gmail.com>
2024-05-18 21:27:48 -0700

    qoustion number 5

---
+++ Q4 famous quote/app.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let person_name = "altaf hussain sajdi";
+console.log(`${person_name}, once said, “A person who never made a mistake never tried anything new.”`);
---
+++ Q4 famous quote/app.ts
@@ -0,0 +1,3 @@
+let person_name:string = "altaf hussain sajdi";
+
+console.log(`${person_name}, once said, “A person who never made a mistake never tried anything new.”`)
