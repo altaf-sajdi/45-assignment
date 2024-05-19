30d172ae476cafb7676e35b1071a04466b112330
altaf-sajdi <sajdiedu@gmail.com>
2024-05-19 10:34:10 -0700

    number 8

---
+++ Q7 number eight/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let Addition = 3 + 5;
+let Subtraction = 10 - 2;
+let Multiplication = 4 * 2;
+let division = 16 / 2;
+console.log('addition,', Addition, 'subtraction,', Subtraction, "multiplication,", Multiplication, "division,", division);
