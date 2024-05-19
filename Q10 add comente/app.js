199f6a618c04aab43991edc4daac2f4e6a59a8dc
altaf-sajdi <sajdiedu@gmail.com>
2024-05-19 11:43:18 -0700

    add  coment

---
+++ Q10 add comente/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Alatf Hussain Sajdi, 2024-05-20
+// This program prints a personal message.
+let myName = "altaf hussain sajdi";
+let meassge = "would you like to learn some TypeScript today?";
+console.log(`Hello ${myName}  ${meassge}, `);
---
+++ Q10 add comente/app.ts
@@ -0,0 +1,6 @@
+// Alatf Hussain Sajdi, 2024-05-20
+// This program prints a personal message.
+let myName: string = "altaf hussain sajdi";
+let meassge:string="would you like to learn some TypeScript today?";
+
+console.log(`Hello ${myName}  ${meassge}, `);
