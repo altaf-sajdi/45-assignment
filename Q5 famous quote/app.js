f1507951773c500dfbbd86fbcfd198f1f37e1f7b
altaf-sajdi <sajdiedu@gmail.com>
2024-05-18 21:19:40 -0700

    qoustion 5 solved

---
+++ Q5 famous quote/app.js
@@ -0,0 +1,3 @@
+var persone_Name = "altaf sajdi";
+var message1 = " ".concat(persone_Name, " once said, \u201CA person who never made a mistake never tried anything new.\u201D");
+console.log(message1);
---
+++ Q5 famous quote/app.ts
@@ -0,0 +1,5 @@
+let persone_Name:string = "altaf sajdi";
+
+let message1: string=` ${persone_Name} once said, “A person who never made a mistake never tried anything new.”`;
+
+console.log(message1)
