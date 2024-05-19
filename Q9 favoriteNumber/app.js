0e85e744f1fdbe7d72d55eddbe19e799c3f5c87b
altaf-sajdi <sajdiedu@gmail.com>
2024-05-19 11:28:03 -0700

    favoriteNumber

---
+++ Q9 favoriteNumber/app.js
@@ -0,0 +1,3 @@
+"use strict";
+let favoriteNumber = 123;
+console.log(`this is my favorite Number=  ${favoriteNumber}`);
---
+++ Q9 favoriteNumber/app.ts
@@ -0,0 +1,3 @@
+let favoriteNumber:number=123;
+
+console.log(`this is my favorite Number=  ${favoriteNumber}`)
