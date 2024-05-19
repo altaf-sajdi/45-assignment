5bfaf7367f0dd3d6c479b587ea5014be01487d67
altaf-sajdi <sajdiedu@gmail.com>
2024-05-18 21:52:50 -0700

    qoustion 6

---
+++ Q6 name trim/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = " \t\n Altaf Hussain Sajdi \t\n";
+console.log(personName);
+console.log(personName.trim());
---
+++ Q6 name trim/app.ts
@@ -0,0 +1,4 @@
+let personName :string =" \t\n Altaf Hussain Sajdi \t\n";
+
+console.log(personName);
+console.log(personName.trim())
