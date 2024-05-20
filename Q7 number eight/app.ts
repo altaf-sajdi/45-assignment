03ef0f7cb8f2c73d923a71304d0c1de8ccf87157
altaf-sajdi <sajdiedu@gmail.com>
2024-05-19 22:39:13 -0700

    change names folders

--- Q7 number eight/app.ts
+++
@@ -1,5 +0,0 @@
- let  Addition:number= 3+5;
- let Subtraction:number=10-2;
- let Multiplication:number=4 * 2;
- let division:number=16 / 2 ;
- console.log('addition,',Addition ,'subtraction,', Subtraction,"multiplication,", Multiplication, "division,", division)
