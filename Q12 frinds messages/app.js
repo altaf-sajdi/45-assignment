ddf1e415fed14cefa5157df7e3745dca8ffc2cde
altaf-sajdi <sajdiedu@gmail.com>
2024-05-20 00:39:22 -0700

    friends message

---
+++ Q12 frinds messages/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let friends = ["altaf", "shahid", "kashif ", "rashid"];
+for (let name of friends) {
+    console.log(`Hello ${name}, are you coming in class today?`);
+}
---
+++ Q12 frinds messages/app.ts
@@ -0,0 +1,4 @@
+let friends: string[] = ["altaf","shahid","kashif ", "rashid"];
+for (let name of friends) {
+    console.log(`Hello ${name}, are you coming in class today?`);
+}
