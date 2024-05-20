5f66014cb171af16b8e8ac4e285f633fd15b1ec9
altaf-sajdi <sajdiedu@gmail.com>
2024-05-20 03:28:45 -0700

    trans

---
+++ Q13 transportatio/app.js
@@ -0,0 +1,4 @@
+var trans = ["car", "bike", "rickshow", "bus"];
+for (var i = 0; i < trans.length; i++) {
+    console.log('i would like to own a ' + trans[i]);
+}
---
+++ Q13 transportatio/app.ts
@@ -0,0 +1,5 @@
+let trans:string[]=["car","bike","rickshow","bus"];
+
+for (let i=0; i<trans.length; i++){
+    console.log('i would like to own a ' + trans[i])
+}
