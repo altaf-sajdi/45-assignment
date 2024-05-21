3bb685e339717832f83aaa71d0d74e5c836af14a
altaf-sajdi <sajdiedu@gmail.com>
2024-05-21 06:37:44 -0700

    guest list

---
+++ Q14 Guest List/app.js
@@ -0,0 +1,11 @@
+"use strict";
+//------------------exmp---------------------
+let guests = ["Albert Einstein", "Marie Curie", "Nikola Tesla"];
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?\n`);
+});
+//----------------my code---------------
+let guestList = ["altaf ", "sajdi", "kausar"];
+guestList.forEach(guestList => {
+    console.log(`Dear ${guestList} , are you come to me \n `);
+});
---
+++ Q14 Guest List/app.ts
@@ -0,0 +1,11 @@
+//------------------exmp---------------------
+let guests: string[] = ["Albert Einstein", "Marie Curie", "Nikola Tesla"];
+guests.forEach(guest => {
+console.log(`Dear ${guest}, would you like to join me for dinner?\n`);});
+
+//----------------my code---------------
+let guestList:string[]= ["altaf ",  "sajdi", "kausar"];
+guestList.forEach(guestList => {
+console.log(`Dear ${guestList} , are you come to me \n `);});
+
+
