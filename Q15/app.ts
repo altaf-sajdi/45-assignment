4b05747b015cc88f541d9a94da18537513bddcef
altaf-sajdi <sajdiedu@gmail.com>
2024-05-28 18:22:01 -0700

    abc

---
+++ Q15/app.ts
@@ -0,0 +1,7 @@
+let guests:string[]=["altaf", "kausar", "nasir", "sajid"];
+guests.forEach(guests =>){
+console.log(`dear ${guests} , would you like to dinner with me !`)
+}
+// replace the guest 
+
+let unableguest:string 
