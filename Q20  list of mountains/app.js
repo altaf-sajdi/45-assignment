898b994a90360173319e6e66016d40d0039ff2d8
altaf-sajdi <sajdiedu@gmail.com>
2024-06-13 05:47:15 -0700

    its Citys off like

---
+++ Q20  list of mountains/app.js
@@ -0,0 +1,3 @@
+"use strict";
+let Citys = ["Karachi", "Lahore", "Mumbai", "New Dehli", "Faisal Abad"];
+console.log("Citys I like to visit:", { Citys });
---
+++ Q20  list of mountains/app.ts
@@ -0,0 +1,4 @@
+
+let Citys: string[] = ["Karachi", "Lahore", "Mumbai", "New Dehli","Faisal Abad"];
+
+console.log("Citys I like to visit:", {Citys});
