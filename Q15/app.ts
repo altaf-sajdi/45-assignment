667bafab05a3161c1ec19109e258b8c728353417
altaf-sajdi <sajdiedu@gmail.com>
2024-06-12 22:28:57 -0700

    guest

--- Q15/app.ts
+++ Q15/app.ts
@@ -1,7 +1,22 @@
-let guests:string[]=["altaf", "kausar", "nasir", "sajid"];
-guests.forEach(guests =>){
-console.log(`dear ${guests} , would you like to dinner with me !`)
-}
-// replace the guest 
+// 14  qoustion
 
-let unableguest:string 
+let guests: string[] = ["Altaf", "Kashif", "Sajid"];
+
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
+
+
+// Q15 Answer:
+let unable = "altaf";
+console.log(`${unable} can't make it to dinner.`);
+
+// Replace
+let newg = "ali";
+guests[guests.indexOf(unable)] = newg;
+
+// New invitations
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
+              
